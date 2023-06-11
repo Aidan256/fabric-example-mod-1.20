dbba20d9415a8ea33ab925cd7d172d6d65c6af97
aidan <addh2005@gmail.com>
2023-06-10 22:37:26 -0400

    committing

---
+++ src/client/java/com/example/SpaceClientMod.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpaceClientMod implements ClientModInitializer {
+    public static final String MOD_ID = "spacemod";
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
